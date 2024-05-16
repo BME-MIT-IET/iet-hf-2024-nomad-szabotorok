296d3e76f950c77c4f61380a2bad489a124a4613
ErikSkare <skareerik55@gmail.com>
2024-05-16 18:23:25 +0200

    Removed not thrown IOException #5

--- src/main/java/hu/bme/mit/iet/pipe_game/Main.java
+++ src/main/java/hu/bme/mit/iet/pipe_game/Main.java
@@ -1,9 +1,7 @@
 package hu.bme.mit.iet.pipe_game;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         /**
          * Létrehozzuk a GUIControl-t, amely a játék grafikus futtatásáért felelős.
          * Ennek a Run függvénye futtatja a játékot(Azaz először a beállításokat, majd a játékot)
