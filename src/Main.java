5511b83eb619128a8befed829ee82d1a4faf6495
tanaiadam <65241679+tanaiadam@users.noreply.github.com>
2024-05-18 17:57:37 +0200

    Auto-UI-testing done, doc added

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        /**
-         * Létrehozzuk a GUIControl-t, amely a játék grafikus futtatásáért felelős.
-         * Ennek a Run függvénye futtatja a játékot(Azaz először a beállításokat, majd a játékot)
-         */
+        // Létrehozzuk a GUIControl-t, amely a játék grafikus futtatásáért felelős.
+        // Ennek a Run függvénye futtatja a játékot(Azaz először a beállításokat, majd a játékot)
         GUIControl control = new GUIControl();
         control.Run();
     }
