cbbe850836e8ac4e1ef12f8cbff5d57ab62fe29d
Peter200204 <peter.ganicz@gmail.com>
2024-05-19 16:19:27 +0200

    PumpTest kesz
    Par hibat eszrevettem az eredeti kodban, azokat meg nem irtam felul, csak kommentben jeleztem

--- pipe_game/src/test/java/hu/bme/mit/iet/PumpTest.java
+++ pipe_game/src/test/java/hu/bme/mit/iet/PumpTest.java
@@ -1,7 +1,8 @@
 package hu.bme.mit.iet;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,7 @@ public class PumpTest {
 
     @Test
     void testAcceptNewFlow() {
+        //megjegyzes: accept new flow hibat dob, amennyiben a from erteke null
         assertEquals(false, pump.AcceptNewFlow(mockPipe1, mockPipe1));//nem lehet ugyabbol ugyanabba vezetni vizet
         assertEquals(true,pump.AcceptNewFlow(mockPipe2, mockPipe1));//2 kulonbozobe viszont igen
     }
@@ -39,35 +41,61 @@ public class PumpTest {
     }
     
     @Test
-    void testPullWater() {
+    void testPullWater() {//megjegyzes: PullWater mukodese elter a kommentbe irttol: MINDIG 0-val ter vissza!
         pump.BreakPump();//pumpa eltorve
-        assertEquals(0,pump.PullWater());//ekkor a behuzott vizmennyiseg 0
-    
+        assertEquals(0,pump.PullWater());//ekkor a beszivott 0
+        pump.Repair();
+        
+        /* megjegyzes: acceptNewFLow hibat dob, ha a from null, mivel meghivja a nullra az equals fgv-t
+         * 
+            pump.AcceptNewFlow(null, mockPipe1);//from null
+            assertEquals(0,pump.PullWater());
+         */
+
+        pump.AcceptNewFlow(mockPipe2, mockPipe1);
+        when(mockPipe2.isBroken()).thenReturn(false);//from is not broken
+        pump.PullWater();
+        verify(mockPipe2,times(1)).getWater();//ha minden rendben el kell kernie a vizt a csobol
+
+
         
     }
 
     @Test
     void testPushWater() {
+        pump.BreakPump();//pumpa eltorve
+        assertEquals(0,pump.PushWater());//ekkor a lyukasson kifolyt 0
+        pump.Repair();
+        
+        /* megjegyzes: acceptNewFLow hibat dob, ha a from null, mivel meghivja a nullra az equals fgv-t
+         * 
+            pump.AcceptNewFlow(mockPipe2, null);//to null
+            assertEquals(0,pump.PullWater());
+         */
+
+        pump.AcceptNewFlow(mockPipe2, mockPipe1);
+        when(mockPipe1.isBroken()).thenReturn(false);//to is not broken
+        pump.PushWater();
+        verify(mockPipe1,times(1)).setWater(anyInt());//ha minden rendben el kell kernie a vizt a csobol
+    }
+
+    @Test
+    void testRepair() {//egyszeru setter
 
     }
 
     @Test
-    void testRepair() {
+    void testGetFrom() {//egyszeru getter
 
     }
 
     @Test
-    void testGetFrom() {
-
+    void testGetTo() {//egyszeru getter
+    
     }
 
     @Test
-    void testGetTo() {
-
-    }
-
-    @Test
-    void testIsBroken() {
+    void testIsBroken() {//egyszeru getter
 
     }
 }
