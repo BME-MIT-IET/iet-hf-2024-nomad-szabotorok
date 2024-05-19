91c4d17a74c56fc1237ca231b819eb5ba9e218d1
Peter200204 <peter.ganicz@gmail.com>
2024-05-19 21:02:59 +0200

    ControlTest added

---
+++ src/test/java/hu/bme/mit/iet/pipe_game/ControlTest.java
@@ -0,0 +1,27 @@
+package hu.bme.mit.iet;
+
+import org.junit.jupiter.api.Test;
+
+public class ControlTest {
+
+
+    @Test
+    void testInit() {
+
+    }
+
+    @Test
+    void testIterateCurrentPlayer() {
+
+    }
+
+    @Test
+    void testPlayerAction() {
+
+    }
+
+    @Test
+    void testRoundOver() {
+
+    }
+}
